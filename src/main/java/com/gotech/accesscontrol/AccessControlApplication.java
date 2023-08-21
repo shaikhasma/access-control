74907a4feba7c95aff79907b9407f0ff0e37d703
ajinkyaswami <ajinkyaswami.as@gmail.com>
2023-08-21 21:30:51 +0530

    ERP-1: swagger implementation & dummy API

--- src/main/java/com/gotech/accesscontrol/AccessControlApplication.java
+++ src/main/java/com/gotech/accesscontrol/AccessControlApplication.java
@@ -3,13 +3,24 @@ package com.gotech.accesscontrol;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.context.annotation.Bean;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
+import static springfox.documentation.builders.RequestHandlerSelectors.basePackage;
+@EnableSwagger2
 @SpringBootApplication(exclude = DataSourceAutoConfiguration.class)
-
 public class AccessControlApplication {
+  public static void main(String[] args) {
+    SpringApplication.run(AccessControlApplication.class, args);
+  }
 
-	public static void main(String[] args) {
-		SpringApplication.run(AccessControlApplication.class, args);
-	}
-
+  @Bean
+  public Docket productApi() {
+    return new Docket(DocumentationType.SWAGGER_2)
+        .select()
+        .apis(basePackage("com.gotech.accesscontrol.controller"))
+        .build();
+  }
 }
---
+++ src/main/java/com/gotech/accesscontrol/controller/AccessController.java
@@ -0,0 +1,41 @@
+package com.gotech.accesscontrol.controller;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/access-control")
+public class AccessController {
+    @GetMapping("/getAll")
+    public ResponseEntity<String> getAllResources() {
+        return new ResponseEntity<String>("get method which returns all resources", HttpStatus.OK);
+    }
+
+    @GetMapping("/get/{id}")
+    public ResponseEntity<String> getResource(@PathVariable int id) {
+        return new ResponseEntity<String>("get method which returns single resource by id", HttpStatus.OK);
+    }
+
+    @PostMapping("/set")
+    public ResponseEntity<String> postResource() {
+        return new ResponseEntity<String>(
+                "post method which sends resource from client", HttpStatus.OK);
+    }
+
+    @PatchMapping("/update/{id}")
+    public ResponseEntity<String> updateResource(@PathVariable int id) {
+        return new ResponseEntity<String>("patch method to update the resource", HttpStatus.OK);
+    }
+
+    @DeleteMapping("/delete/{id}")
+    public ResponseEntity<String> deleteResource(@PathVariable int id) {
+        return new ResponseEntity<String>("delete method which deletes resources", HttpStatus.OK);
+    }
+}
