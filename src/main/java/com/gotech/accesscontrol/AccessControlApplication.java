ef1c7db3f11f2dcb24a6eba4117b0ae99b2a65c6
shaikhasma <shaikh.asma9999@gmail.com>
2023-08-21 08:08:47 +0530

    Project Structure

---
+++ src/main/java/com/gotech/accesscontrol/AccessControlApplication.java
@@ -0,0 +1,16 @@
+package com.gotech.accesscontrol;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+
+@SpringBootApplication(exclude = DataSourceAutoConfiguration.class)
+
+public class AccessControlApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AccessControlApplication.class, args);
+	}
+
+}
