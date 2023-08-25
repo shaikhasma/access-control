3f016b4a599ba342d9b75aacc06082310590a5e5
ajinkyaswami <ajinkyaswami.as@gmail.com>
2023-08-25 14:25:40 +0530

    @service annotation

--- src/main/java/com/gotech/accesscontrol/service/UserService.java
+++ src/main/java/com/gotech/accesscontrol/service/UserService.java
@@ -2,7 +2,9 @@ package com.gotech.accesscontrol.service;
 
 import com.gotech.accesscontrol.repository.UserRepo;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
+@Service
 public class UserService {
 
     @Autowired
