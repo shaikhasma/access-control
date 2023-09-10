82b658e58cc3ac79f6cd413c4422fd0433853feb
ajinkyaswami <ajinkyaswami.as@gmail.com>
2023-09-10 15:14:28 +0530

    Test

--- src/test/java/com/gotech/accesscontrol/service/UserServiceImplTest.java
+++ src/test/java/com/gotech/accesscontrol/service/UserServiceImplTest.java
@@ -12,10 +12,16 @@ import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.http.HttpStatus;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import static com.gotech.accesscontrol.constant.Constants.created;
+import static com.gotech.accesscontrol.constant.Constants.duplicate;
+import static com.gotech.accesscontrol.constant.Constants.invalidUser;
 import static com.gotech.accesscontrol.util.UserMapper.toUser;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.anyString;
 import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
@@ -26,27 +32,26 @@ public class UserServiceImplTest {
     private UserRepo userRepo;
     @InjectMocks
     private UserServiceImpl userServiceImpl;
+    private final UserRequest userRequest = UserRequest.builder()
+            .firstName("Vinay")
+            .lastName("Shinde")
+            .mobileNumber("8847592130")
+            .emailId("vs@gmail.com")
+            .password("vshinde@1234")
+            .userType(UserType.BRANCH_MANAGER)
+            .build();
+
+    private final User user = User.builder()
+            .firstName(userRequest.getFirstName())
+            .lastName(userRequest.getLastName())
+            .mobileNumber(userRequest.getMobileNumber())
+            .emailId(userRequest.getEmailId())
+            .password(userRequest.getPassword())
+            .userType(userRequest.getUserType())
+            .build();
 
     @Test
     public void testSaveUser() {
-        UserRequest userRequest = UserRequest.builder()
-                .firstName("Vinay")
-                .lastName("Shinde")
-                .mobileNumber("8847592130")
-                .emailId("vs@gmail.com")
-                .password("vshinde@1234")
-                .userType(UserType.BRANCH_MANAGER)
-                .build();
-
-        User user = User.builder()
-                .firstName(userRequest.getFirstName())
-                .lastName(userRequest.getLastName())
-                .mobileNumber(userRequest.getMobileNumber())
-                .emailId(userRequest.getEmailId())
-                .password(userRequest.getPassword())
-                .userType(userRequest.getUserType())
-                .build();
-
         Response successResponse = Response.builder()
                 .statusCode(HttpStatus.CREATED.toString())
                 .statusMessage(created + user)
@@ -58,5 +63,52 @@ public class UserServiceImplTest {
         assertEquals(user, user1);
         assertEquals(response, successResponse);
         verify(userRepo, times(1)).save(any(User.class));
+    }
+
+    @Test
+    public void testSaveDuplicateUser() {
+        Response failureResponse = Response.builder()
+                .statusCode(HttpStatus.CONFLICT.toString())
+                .statusMessage(duplicate + user)
+                .build();
+
+        List<User> savedUsers = new ArrayList<>();
+        savedUsers.add(user);
+        when(userRepo.findUserByEmailAndMobileNumber(anyString(), anyString())).thenReturn(savedUsers);
+        savedUsers = userRepo.findUserByEmailAndMobileNumber(user.getEmailId(), user.getMobileNumber());
+        User user2 = savedUsers.get(0);
+        User user1 = toUser(userRequest);
+        Response response = userServiceImpl.saveUser(userRequest);
+        assertEquals(user, user1);
+        assertEquals(user, user2);
+        assertEquals(response, failureResponse);
+    }
+
+    @Test
+    public void testSaveInvalidUser() {
+        UserRequest testInvalidUser = UserRequest.builder()
+                .firstName("Vinay")
+                .lastName("Shinde")
+                .mobileNumber("8847592130")
+                .emailId("vsgmail.com")
+                .password("vshinde@1234")
+                .userType(UserType.BRANCH_MANAGER)
+                .build();
+
+        User user1 = toUser(testInvalidUser);
+
+        Response failureResponse = Response.builder()
+                .statusCode(HttpStatus.UNPROCESSABLE_ENTITY.toString())
+                .statusMessage(invalidUser + user1)
+                .build();
+
+        Response response = userServiceImpl.saveUser(testInvalidUser);
+        assertEquals(user1.getFirstName(), testInvalidUser.getFirstName());
+        assertEquals(user1.getLastName(), testInvalidUser.getLastName());
+        assertEquals(user1.getMobileNumber(), testInvalidUser.getMobileNumber());
+        assertEquals(user1.getEmailId(), testInvalidUser.getEmailId());
+        assertEquals(user1.getPassword(), testInvalidUser.getPassword());
+        assertEquals(user1.getUserType(), testInvalidUser.getUserType());
+        assertEquals(response, failureResponse);
     }
 }
